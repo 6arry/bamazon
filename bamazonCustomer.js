824a35512f8024b51fdf5371cad9a68e347ba44c
gccunanan <gccunanan@gmail.com>
2019-07-07 13:48:43 -0700

    bamazonCustomer.js updated

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host:"localhost",
     port:3306,
     user:"root",
-    password:"NotAnother1",
+    password:"YOURPASSWORDHERE",
     database:"bamazon"
 })
 
